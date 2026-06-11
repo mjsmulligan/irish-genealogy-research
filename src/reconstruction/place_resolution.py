c97c59884ccca50c8c40a32a8506de00d44fbd26
mjsmulligan <mjsmulligan@gmail.com>
2026-06-11 23:45:28 +0100

    Replace jellyfish with rapidfuzz for similarity scoring

--- src/reconstruction/place_resolution.py
+++ src/reconstruction/place_resolution.py
@@ -163,7 +163,7 @@ def _best_match(
     best_score = 0.0
     for auth in authorities:
         for auth_norm in auth["norms"]:
-            score = jellyfish.jaro_winkler_similarity(norm, auth_norm)
+            score = rapidfuzz.jaro_winkler_similarity(norm, auth_norm)
             if score > best_score:
                 best_score = score
                 best_auth = auth
